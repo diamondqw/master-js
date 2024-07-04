7664d854adafa45a52d0e76a601e3b842ac7875a
pawanpediredla <150579399+pawanpediredla@users.noreply.github.com>
2024-07-04 20:56:12 +0530

    Update index.js

--- setInterval/index.js
+++ setInterval/index.js
@@ -5,3 +5,19 @@ let timeId  =  setInterval(callBack,2000)
 console.log(timeId);
 
 clearInterval(timeId)
+
+function startInterval() {
+  var delay = 1000; // Initial interval delay
+
+  intervalID = setInterval(function() {
+    console.log('Interval executed');
+    // Example of conditionally adjusting interval
+    if (conditionToChangeInterval) {
+      clearInterval(intervalID); // Clear previous interval
+      delay = 500; // New interval delay
+      intervalID = setInterval(arguments.callee, delay); // Restart interval with new delay
+    }
+  }, delay);
+}
+
+startInterval();
