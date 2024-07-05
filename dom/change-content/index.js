e4beada7387189f61bf69cbb68bb707683107d13
pawanpediredla <150579399+pawanpediredla@users.noreply.github.com>
2024-07-05 18:49:04 +0530

    Update index.js

--- dom/change-content/index.js
+++ dom/change-content/index.js
@@ -3,3 +3,6 @@ ascel.textContent ="chanage content"
 
 const ascsel1  =  document.getElementById('.fck1')
 ascsel1.innerHtml('<p>New Content</p>')
+
+const ascel2  = document.getElementById('.fck2')
+ascel2.innerText ="change content"
