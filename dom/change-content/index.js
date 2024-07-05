2a5f56fbc99d22c11752931e34ae01f32288b1e3
pawanpediredla <150579399+pawanpediredla@users.noreply.github.com>
2024-07-05 18:58:15 +0530

    Update index.js

--- dom/change-content/index.js
+++ dom/change-content/index.js
@@ -8,3 +8,16 @@ const ascel2  = document.getElementById('.fck2')
 ascel2.innerText ="change content"
 const imgL =document.getElementById('.img1')
 imgL.setAttribute('src','newImage.png')
+//styling
+ascel1.style.color='red'
+ascel1.addClassList('.hightlight')
+
+//add or remove element
+
+const nwEl =document.createElement('div')
+
+acsel=document.appendChild(nwEl)
+
+ascel.removeChild(nwEl)
+
+const 
