bfe67dfa14e993658d57999542b9fe73e5ab48dd
pawanpediredla <150579399+pawanpediredla@users.noreply.github.com>
2024-07-05 12:24:43 +0530

    Update index.js

--- dom/dom-manipulation/index.js
+++ dom/dom-manipulation/index.js
@@ -1,7 +1,11 @@
+document.addEventListener('DOMContentLoaded',function(){
 let headingElement = document.getElementById("main-heading")
 let paraElement  = document.getElementById("paragraph")
 let buttonLElement  = document.getElementById("change-text-button")
 buttonElement.addEventListener('click',function(){
   headingElement.textContent ="changed"
   paraElement.classList.add("hightlight")
+})
+
+  
 })
