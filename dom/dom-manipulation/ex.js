f96b0a0c3c47857dc2950e6fe34e8f03af6150a1
pawanpediredla <150579399+pawanpediredla@users.noreply.github.com>
2024-07-05 12:34:43 +0530

    returns elemtns with node list css selecotrs matching

--- dom/dom-manipulation/ex.js
+++ dom/dom-manipulation/ex.js
@@ -14,3 +14,13 @@ getElementByTagName
 //
 let getTag =document.getElementByTagName('<div>')
 
+
+// return element with css selectors //
+
+let getSls = document.querySelector('.feckquery')
+
+//
+
+// returns elemtns with node list css selecotrs matching //
+let getSlsalls  =document.querySelectorAll('.feckqueryAll')
+
