cf909f5e3405d10e7a059dffab550564e6fa1267
pawanpediredla <150579399+pawanpediredla@users.noreply.github.com>
2024-07-05 12:30:23 +0530

    Create ex.js

---
+++ dom/dom-manipulation/ex.js
@@ -0,0 +1,16 @@
+//
+getElementByid //
+
+let get = document.getElementById('feck')
+
+//
+getElementByClassName 
+//
+let getcs  =  document.getElementByClassName('.feck2')
+
+//
+getElementByTagName
+
+//
+let getTag =document.getElementByTagName('<div>')
+
