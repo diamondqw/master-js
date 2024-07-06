f6eedca4f117d219a7044ebdde4d297d4ed4de8f
pawanpediredla <150579399+pawanpediredla@users.noreply.github.com>
2024-07-06 07:49:45 +0530

    update dom addEventListener

--- dom/add-evenListener/mouse-over/index.js
+++ dom/add-evenListener/mouse-over/index.js
@@ -1,1 +1,9 @@
-let cont   = document.getElementById('')
+let cont   = document.getElementById('dv');
+dv.addEventListener('mouseover' ,function(event){
+  dv.textContent='mouse over me ';
+  dv.style.backgroundColor ='red'
+})
+dv.addEventListener('mouseout',function(event){
+  dv.textContent = 'hover me '
+  dv.style.backgroundColort ='yellow'
+})
