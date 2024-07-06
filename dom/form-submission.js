6248c5954a4ef0b82a54d90433a9c9953f2f81d3
pawanpediredla <150579399+pawanpediredla@users.noreply.github.com>
2024-07-06 09:40:58 +0530

    Update form-submission.js

--- dom/form-submission.js
+++ dom/form-submission.js
@@ -1,12 +1,21 @@
 // html
 <form id="formj">
-<input type="text" placeholder="enter something"/>
+<input type="text" id="textinp" placeholder="enter something"/>
 <button type="submit"> submit it </button>
 </form>
 // js 
-const inpEl = document.getElementById('formj')
-formj.addEventListener('submit' , function(event){
+let formx = document.getElemenyId("formj")
+formx.addEventListener('submit' , function(event){
 event.preventDefault();
+  let inpfield = document.getElementById('textinp')
+  const value = inpfield.value;
+  if(inpfield.trim()====null){
+    alert("enter something");
+    return;
+  }
+
+  alert(value);
+  
 
 
 })
