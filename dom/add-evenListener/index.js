3c1aabae79f92273edd7a60df7aeb88546fe1dce
pawanpediredla <150579399+pawanpediredla@users.noreply.github.com>
2024-07-06 07:30:16 +0530

    making button invisible

--- dom/add-evenListener/index.js
+++ dom/add-evenListener/index.js
@@ -2,3 +2,8 @@ let btnEl  = document.getElementById('buttonn')
 btnEl.addEventListener('click',function(event){
   alert("jss  s s s s ")
 })
+let btnEl  = document.getElementById('buttonn')
+btnEl.addEventListener('click',function(event){
+
+  document.body.removeChild(btnEl)
+})
