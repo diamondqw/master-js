48d5bfb58814b0695fab4a0f9732b8a1dce095d4
diamondqw <150579399+diamondqw@users.noreply.github.com>
2024-07-07 19:31:04 +0530

    protoType inheritance

--- prototype/index.js
+++ prototype/index.js
@@ -3,3 +3,10 @@ function Person(name){
 }
 let person1 = new Person("paawan")
 console.log(person1.__proto__ === Person.prototype)
+
+//protoype inheritance 
+
+Person.prototype.greet=function(){
+  return `hi ${this.name}`
+}
+console.log(person1.greet)
